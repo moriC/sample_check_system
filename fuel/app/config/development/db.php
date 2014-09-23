55e7220efd8d8a36873cfef6538d39352e68d34d
morishita <tomohiro.m0219@gmail.com>
2014-09-23 18:24:50 +0900

    データベース接続設定記述 #1
    データベースに接続するためのデータベース情報、ユーザ情報を記述

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev',
+			'dsn'        => 'mysql:host=localhost;dbname=scs_dev',
 			'username'   => 'root',
-			'password'   => 'root',
+			'password'   => '',
 		),
 	),
 );
